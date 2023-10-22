68b424860bd7fb81d505cd10054c9e302050cd67
kiatklai <likhitthaworn.k@gmail.com>
2023-10-22 20:38:23 +0900

    admin設定/モデル作成/モデルでテーブルのように表示

--- productapp/admin.py
+++ productapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from productapp.models import Product
+
+class ManageProduct(admin.ModelAdmin):
+  list_display=["name","price","stock","isTrending"] #listでobjectの形を見やすいようにする
+  list_editable=["price","stock","isTrending"]  #listで直接修正できる
+  list_per_page=5  #listの1ページでいくらの数を表示したいか
+  search_fields=["name"]  #商品名(name)で検索できるinputがある
+  list_filter=["isTrending"]  #checkが入っていたら、当てはまる商品が表示される
 
 # Register your models here.
+admin.site.register(Product,ManageProduct)
