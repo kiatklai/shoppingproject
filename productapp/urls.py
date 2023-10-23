21aba35f55e7eaa177ba97e108e36289a12f5b36
kiatklai <likhitthaworn.k@gmail.com>
2023-10-23 21:13:06 +0900

    全て商品の情報を取得/ページ番号作成

--- productapp/urls.py
+++ productapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from productapp import views
 
 urlpatterns=[
-  path("",views.index)
+  path("",views.index),
+  path("product/detail/<int:id>",views.productDetail,name="productDetail"),
+  path("products",views.products)
 ]
--- productapp/views.py
+++ productapp/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from productapp.models import Product
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
   products=Product.objects.filter(isTrending=True)
   return render(request,"index.html",{"products":products})
+
+def productDetail(request,id):
+  product=Product.objects.get(pk=id)
+  return render(request,"detail.html",{"product":product})
+
+def products(request):
+  all_products=Product.objects.all()
+  #pageの番号を設定
+  page = request.GET.get("page")
+  paginator=Paginator(all_products,3) #1 pageにいくらの商品を表示させるか
+  all_products=paginator.get_page(page)
+  return render(request,"products.html",{"all_products":all_products})
